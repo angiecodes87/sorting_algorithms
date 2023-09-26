59a912c354813df06b872aa45e579d0c1cc0a60c
Angiecodes <angelcoder87@gmail.com>
2023-09-26 17:10:22 +0300

    Bubble sort

---
+++ print_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * custom_print_array - Display an integer array
+ *
+ * @arr: The array to be displayed
+ * @len: Number of elements in @arr
+ */
+void custom_print_array(const int *arr, size_t len)
+{
+	size_t index;
+
+	index = 0;
+	while (arr && index < len)
+	{
+		if (index > 0)
+		printf(", ");
+		printf("%d", arr[index]);
+		++index;
+	}
+	printf("\n");
+}
+
